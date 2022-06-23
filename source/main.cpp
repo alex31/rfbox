ef03fb12cb95a2512f7b7ad50f7d9fc39eaeee55
Alexandre Bustico <alexandre.bustico@enac.fr>
2022-06-23 18:21:58 +0200

    refresh gray code in ISR instead of polling

--- source/main.cpp
+++ source/main.cpp
@@ -61,31 +61,49 @@ static THD_WORKING_AREA(waTraceLptim1, 304);
   }
 
   palClearLine(LINE_NOT_ZEROED);
-  uint8_t lastCode = 0xFF;
 
-  while (true) {				
-    auto [u, cnt] = lptim1d.getCnt();
-    if (u) {
-      cnt = (cnt + cntOffset) % 2048;
-      const uint8_t angle = cnt >> 5;
-      const uint8_t grayCode = binaryToGray(angle);
-      if (lastCode != grayCode) {
-	lastCode = grayCode;
-	uint8_t portGrayCode = grayCode << 2;
-	// dispatch 6 bits code on Port A : 1,3,4,5,6,7
-	(portGrayCode |= ((portGrayCode & 0b100) >> 1)) &= 0b11111010;
-	// atomic 6 bits gray code update
-	palWriteGroup(GPIOA, 0b11111010, 0, portGrayCode);
-#ifndef NOSHELL
-	std::string binGray(binary_fmt(grayCode, 8));
-	std::string binPort(binary_fmt(portGrayCode, 8));
-	DebugTrace("lptim1 CNT = %u [%u] [g:0x%x -> 0x%x] {%s -> %s}",
-		   cnt, angle, grayCode, portGrayCode,
-		   binGray.c_str(), binPort.c_str());
-#endif
-      }
+  auto cb =  [] (void *) {
+    static uint8_t lastCode = 0xFF;
+    auto [_, cnt] = lptim1d.getCnt();
+    cnt = (cnt + cntOffset) % 2048;
+    const uint8_t angle = cnt >> 5;
+    const uint8_t grayCode = binaryToGray(angle);
+    if (lastCode != grayCode) {
+      lastCode = grayCode;
+      uint8_t portGrayCode = grayCode << 2;
+      // dispatch 6 bits code on Port A : 1,3,4,5,6,7
+      (portGrayCode |= ((portGrayCode & 0b100) >> 1)) &= 0b11111010;
+      // atomic 6 bits gray code update
+      palWriteGroup(GPIOA, 0b11111010, 0, portGrayCode);
     }
-    chThdSleepMicroseconds(100);
+  };
+
+  palEnableLineEvent(LINE_WIND_DIR_CHA_IT, PAL_EVENT_MODE_FALLING_EDGE);
+  palSetLineCallback(LINE_WIND_DIR_CHA_IT, cb, NULL);
+  
+#ifdef NOSHELL
+  chThdSleep(TIME_INFINITE);
+#endif
+
+  uint8_t lastCode = 0xFF;
+  while (true) {				
+    auto [_, cnt] = lptim1d.getCnt();
+    cnt = (cnt + cntOffset) % 2048;
+    const uint8_t angle = cnt >> 5;
+    const uint8_t grayCode = binaryToGray(angle);
+    if (lastCode != grayCode) {
+      lastCode = grayCode;
+      uint8_t portGrayCode = grayCode << 2;
+      // dispatch 6 bits code on Port A : 1,3,4,5,6,7
+      (portGrayCode |= ((portGrayCode & 0b100) >> 1)) &= 0b11111010;
+      // atomic 6 bits gray code update
+      std::string binGray(binary_fmt(grayCode, 8));
+      std::string binPort(binary_fmt(portGrayCode, 8));
+      DebugTrace("lptim1 CNT = %u [%u] [g:0x%x -> 0x%x] {%s -> %s}",
+		 cnt, angle, grayCode, portGrayCode,
+		 binGray.c_str(), binPort.c_str());
+    }
+    chThdSleepMilliseconds(100);
   }
 }
 
